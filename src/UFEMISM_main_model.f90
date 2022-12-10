423ff81d20fef7780771c32a66645286d31be656
Jorjo Bernales <j.a.bernalesconcha@uu.nl>
2022-12-10 12:42:20 +0100

    Remove ocean dependence in thermo routines
    Ocean is not used anymore, but some routines were expecting it,
    causing an error during compilation. Fixed it.

--- src/UFEMISM_main_model.f90
+++ src/UFEMISM_main_model.f90
@@ -202,7 +202,7 @@ CONTAINS
       ! =================
 
       t1 = MPI_WTIME()
-      CALL run_thermo_model( region%mesh, region%ice, region%climate_matrix%applied, region%ocean_matrix%applied, region%SMB, region%BMB, region%time, do_solve_heat_equation = region%do_thermo)
+      CALL run_thermo_model( region%mesh, region%ice, region%climate_matrix%applied, region%SMB, region%BMB, region%time, do_solve_heat_equation = region%do_thermo)
       t2 = MPI_WTIME()
       IF (par%master) region%tcomp_thermo = region%tcomp_thermo + t2 - t1
 
@@ -613,7 +613,7 @@ CONTAINS
     CALL run_SMB_model( region%mesh, region%ice, region%climate_matrix, C%start_time_of_run, region%SMB, region%mask_noice)
 
     ! Initialise the ice temperature profile
-    CALL initialise_ice_temperature( region%mesh, region%ice, region%climate_matrix%applied, region%ocean_matrix%applied, region%SMB, region%name, region%restart)
+    CALL initialise_ice_temperature( region%mesh, region%ice, region%climate_matrix%applied, region%SMB, region%name, region%restart)
 
     ! Initialise the rheology
     CALL calc_ice_rheology( region%mesh, region%ice, C%start_time_of_run)
